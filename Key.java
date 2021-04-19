8ee4b31254b54a4609da9074e01b59efb945f9fa
peytond <peyton.dunn@stu.boone.kyschools.us>
2021-04-19 08:39:10 -0400

    added 2 sounds

--- Key.java
+++ Key.java
@@ -2,11 +2,17 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, and Greenfoot)
 
 public class Key extends Actor
 {
+    private boolean isDown;
+    private String key;
+    private String sound;
+    
     /**
      * Create a new key.
      */
-    public Key()
+    public Key(String keyName, String soundFile)
     {
+        key = keyName;
+        sound = soundFile;
     }
 
     /**
@@ -14,5 +20,23 @@ public class Key extends Actor
      */
     public void act()
     {
+        if(!isDown && Greenfoot.isKeyDown(key))
+        {
+            setImage("white-key-down.png");
+            play();
+            isDown=true;
+        }
+        if(isDown && !Greenfoot.isKeyDown(key))
+        {
+            setImage("white-key.png");
+            isDown=false;
+        }
+    }
+    /*
+     * Play the note for this key
+     */
+    public void play()
+    {
+        Greenfoot.playSound(sound);
     }
 }
